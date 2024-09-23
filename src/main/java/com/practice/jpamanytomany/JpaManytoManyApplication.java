24bd77e603a12d59bbdd0d4f2daf20fd4f83c69d
Pavan Ambi <pavan.fly@gmail.com>
2024-09-23 12:18:38 -0400

    JPA examples with spring boot

---
+++ src/main/java/com/practice/jpamanytomany/JpaManytoManyApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jpamanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManytoManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaManytoManyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/jpamanytomany/JpaManytoManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jpamanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaManytoManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
